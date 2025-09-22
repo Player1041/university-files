1d0276fd892f81051b38654d53f27571a695134c
Sean <redacteddoesit@gmail.com>
2025-09-22 15:24:04 +0100

    helloworld
    hello world example 1

---
+++ cpp/source/HelloUniverse/HelloUniverse/main.cpp
@@ -0,0 +1,14 @@
+// First program in Game Programming Fundamentals
+// 22/09/25
+
+#include <iostream> // input/output streaming library
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl; //console output << print here << end line
+	return 0; // return 0 as success
+}
+
+// EOF
