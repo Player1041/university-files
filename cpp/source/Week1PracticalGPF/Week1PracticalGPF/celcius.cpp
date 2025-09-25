1d8dc9e39105323903731347057ea27c496bf821
Sean <redacteddoesit@gmail.com>
2025-09-25 09:49:49 +0100

    week 1 gpf stuff

---
+++ cpp/source/Week1PracticalGPF/Week1PracticalGPF/celcius.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float f;
+    float c;
+
+	cout << "Input fahrenheit" << endl;
+	cin >> f;
+
+	c = (f - 32) * 5 / 9;
+	cout << f << " Fahrenheit in Celsius is " << c << endl;
+
+}
---
+++ cpp/source/Week1PracticalGPF/Week1PracticalGPF/maths.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main2() {
+	int value1;
+	cout << "Input a number" << endl;
+	cin >> value1;
+
+	int value2;
+	cout << "Input another number" << endl;
+	cin >> value2;
+
+	int result = value1 / value2;
+
+	cout << "The result is " << result << endl;
+
+	return 0;
+}
